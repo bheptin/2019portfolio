a9d5391fadb9ac9cfdd0fbd8c18bf6d4329f20cf
Brian H <bheptin@gmail.com>
2019-04-02 16:50:25 -0500

    add React files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="opening-link"
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Brian Heptinstall...a brief history
+            </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
